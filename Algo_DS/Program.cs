c2a216b2b0e7398033fde1657b0b7dda00f57b9f
Sreeharsha Parankusham <harsha2031@gmail.com>
2020-09-16 09:55:46 -0500

    First Commit

--- Algo_DS/Program.cs
+++ Algo_DS/Program.cs
@@ -6,7 +6,7 @@ namespace Algo_DS
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("World!");
         }
     }
 }
