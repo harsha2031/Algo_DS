5f714093e6cd59528773d756fc3cda59bf7f05e0
Sreeharsha Parankusham <harsha2031@gmail.com>
2020-11-20 14:13:00 -0600

    Daily Coding Problem Started.

---
+++ Algo_DS/Models/TreeNode.cs
@@ -0,0 +1,16 @@
+﻿using System;
+namespace Algo_DS.Models
+{
+    public class TreeNode
+    {
+        public int value;
+        public TreeNode left;
+        public TreeNode right;
+        public TreeNode(int val)
+        {
+            this.value = val;
+            this.left = null;
+            this.right = null;
+        }
+    }
+}
